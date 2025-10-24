395af771c5fe2ff376820e55a9902c06f6fffee5
lastpi1ot <hamzaett1320@gmail.com>
2025-10-24 23:40:59 +0100

    Initial commit

---
+++ src/main/java/com/example/studentmanagement/repositories/StudentRepository.java
@@ -0,0 +1,14 @@
+package com.example.studentmanagement.repositories;
+
+import com.example.studentmanagement.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+
+    @Query("SELECT YEAR(s.dateNaissance), COUNT(s) FROM Student s GROUP BY YEAR(s.dateNaissance)")
+    Collection<Object[]> findNbrStudentByYear();
+}
